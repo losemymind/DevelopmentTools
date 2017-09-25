13e13092b9d639213902dc6cc70f8efde4b6a102
losemymind <losemymind.libo@gmail.com>
2017-09-25 13:47:42 +0800

    no message

--- Valgrind/start_valgrind.sh
+++
@@ -1,6 +0,0 @@
-#!/system/bin/sh
-PACKAGE="com.snailgame.tjyd"  
-VGPARAMS='-v --error-limit=no --trace-children=yes --track-fds=yes --log-file=/sdcard/$PACKAGE.%p.valgrind.log --tool=memcheck --leak-check=full --track-origins=yes'  
-export TMPDIR=/data/data/$PACKAGE  
-echo "Executing through valgrind: $VGPARAMS $*"
-exec /data/local/Inst/bin/valgrind $VGPARAMS $*
